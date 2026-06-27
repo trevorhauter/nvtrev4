5b88d60aae0f3b780e88dd30663b702bcc3211e6
Trevor Hauter <trevor@hauter.dev>
2026-06-27 19:16:09 -0400

    Add scaffolding files

---
+++ lua/plugins/init.lua
@@ -0,0 +1,12 @@
+-- Require all files in directory so I can just do require("plugins")
+
+local config_path = vim.fn.stdpath("config")
+local directory_name = "plugins"
+
+for _, file in ipairs(vim.fn.readdir(config_path .. "/lua/" .. directory_name, [[v:val =~ '\.lua$']])) do
+    local file = file:gsub("%.lua$", "")
+
+    if file ~= "init" then
+        require(directory_name .. "." .. file)
+    end
+end
