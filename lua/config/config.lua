98599540c7770f76d3c1afe665ab2bacce44e2bf
Trevor Hauter <trevor@hauter.dev>
2026-06-27 23:54:25 -0400

    Configure settings for neovim

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -1,1 +1,17 @@
 require("plugins")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.diagnostic.config({ virtual_text = true }) -- lsp warnings
+
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2
+
+vim.opt.expandtab = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.opt.scrolloff = 8
