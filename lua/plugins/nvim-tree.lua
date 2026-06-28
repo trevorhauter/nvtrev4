d7bed11b9506dce430fbdd2bc50833f7e78420dd
Trevor Hauter <trevor@hauter.dev>
2026-06-28 00:39:04 -0400

    Add more plugins

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,20 @@
+vim.pack.add({
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+})
+
+-- Turn off netrw because nvim-tree replaces it
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+    view = {
+        adaptive_size = true,
+        number = true,
+        relativenumber = true,
+    },
+    filters = { custom = { "^.git$", "^.gitignore$" } },
+})
+
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>n", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
