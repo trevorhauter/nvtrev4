8a895269962b7d8c6297a5bc34f1c90747bba8b4
Trevor Hauter <trevor@hauter.dev>
2026-06-29 10:11:02 -0400

    Add leap.nvim

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,40 @@
+vim.pack.add({
+    "https://codeberg.org/andyg/leap.nvim",
+    "https://github.com/tpope/vim-repeat",
+})
+
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+
+-- Recommended additional config for LEAP
+-- define a preview filter to reduce visual noise
+-- and the blinking effect after the first keypress.
+-- For example, define word boundaries as the common case, that is, skip
+-- preview for matches starting with whitespace or an alphabetic
+-- mid-word character: foobar[baaz] = quux
+--                     ^    ^^^  ^^ ^ ^  ^
+require("leap").opts.preview = function(ch0, ch1, ch2)
+    return not (
+        ch1:match("%s")
+        or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
+    )
+end
+
+-- Enable the traversal keys to repeat the previous search without
+-- explicitly invoking Leap (`<cr><cr>...` instead of `s<cr><cr>...`):
+do
+    local clever = require("leap.user").with_traversal_keys
+    vim.keymap.set({ "n", "x", "o" }, "<cr>", function()
+        require("leap").leap({
+            ["repeat"] = true,
+            opts = clever("<cr>", "<bs>"),
+        })
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "<bs>", function()
+        require("leap").leap({
+            ["repeat"] = true,
+            opts = clever("<bs>", "<cr>"),
+            backward = true,
+        })
+    end)
+end
