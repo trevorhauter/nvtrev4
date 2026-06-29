5bc3f3e205daf8ecd094cb5823c4b38c54066749
Trevor Hauter <trevor@hauter.dev>
2026-06-29 11:13:07 -0400

    Change markdown rendering style

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,3 +1,7 @@
 vim.pack.add({ "https://github.com/MeanderingProgrammer/render-markdown.nvim" })
 
-require("render-markdown").setup()
+require("render-markdown").setup({
+    heading = {
+        width = "block",
+    },
+})
