012a6be305a3d6746474b5b992284cc6332e3a0a
Trevor Hauter <trevor@hauter.dev>
2026-07-01 09:14:03 -0400

    Remove unused keybind

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -15,10 +15,3 @@ require("treesitter-context").setup({
     zindex = 20, -- The Z-index of the context window
     on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 })
-
-vim.keymap.set(
-    "n",
-    "<C-a>",
-    ":TSContextToggle<CR>",
-    { noremap = true, silent = true }
-)
