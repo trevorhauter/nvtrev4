85eec2191360045962d61834d18efe41e3307062
Trevor Hauter <trevor@hauter.dev>
2026-07-01 16:06:03 -0400

    Improve styling of fff.nvim

--- lua/config/appearance.lua
+++ lua/config/appearance.lua
@@ -2,8 +2,9 @@
 -- prevent conflicts.
 
 -- Customize appearance of fff.nvim
-vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
-vim.api.nvim_set_hl(0, "FloatBorder", { link = "FloatBorder" })
+-- TODO: This should be configurable via fff.nvim config options. Send a patch
+-- upstream to fix this.
+vim.api.nvim_set_hl(0, "IncSearch", { bg = "#afff5f" })
 
 -- Customize apperance of eyeline.nvim
 vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#afff5f", bold = true })
