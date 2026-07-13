6980557377ff718e13cb970721a2770062b97b41
Trevor <trevor@utilityapi.com>
2026-07-13 15:36:50 -0400

    Remove appearance config that was for fff

--- lua/config/appearance.lua
+++ lua/config/appearance.lua
@@ -9,15 +9,6 @@ vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "FloatBorder" })
 vim.api.nvim_set_hl(0, "FloatTitle", { link = "Title" })
 
--- Customize appearance of fff.nvim
--- TODO: This should be configurable via fff.nvim config options. Send a patch
--- upstream to fix this.
-vim.api.nvim_set_hl(
-    0,
-    "IncSearch",
-    { bg = "#6d6b6b", fg = bright_green, underline = true }
-)
-
 -- Customize apperance of eyeline.nvim
 vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = bright_green, bold = true })
 vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = bright_blue })
