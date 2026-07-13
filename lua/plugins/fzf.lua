339b7c02f63b016a9e21cd6befcf8984cbf78fe4
Trevor <trevor@utilityapi.com>
2026-07-13 16:19:43 -0400

    Remove unnecessary config for fzf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,8 +2,6 @@ vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" })
 
 local fzf = require("fzf-lua")
 
-fzf.setup({ { "telescope" } })
-
 vim.keymap.set({ "n", "v" }, "<leader>/", fzf.live_grep)
 vim.keymap.set({ "n", "v" }, "<leader>d", fzf.git_status)
 vim.keymap.set({ "n", "v" }, "<leader>f", fzf.files)
