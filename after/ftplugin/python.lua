0f7859fdec8f7dbf74303578f7432f791d7426bf
Trevor <trevor@utilityapi.com>
2026-07-24 15:14:28 -0400

    Add django test keybind

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,3 +1,13 @@
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
+
+local function get_visual_selection()
+    -- Yank the highlighted text then pull it out of the register
+    vim.cmd.normal({ '"zy', bang = true })
+    local selection = vim.fn.getreg("z")
+    vim.cmd("TermExec cmd='djt " .. selection .. "'")
+end
+
+-- Django mapping to run tests easily in visual mode
+vim.keymap.set("v", "<cr><cr>", get_visual_selection)
