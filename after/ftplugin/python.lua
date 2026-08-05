c8a455fcd86c0b029e6a5823cd1b0832dd3a7604
Trevor <trevor@utilityapi.com>
2026-08-05 13:28:00 -0400

    Refactor test runner command

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -6,8 +6,13 @@ local function get_visual_selection()
     -- Yank the highlighted text then pull it out of the register
     vim.cmd.normal({ '"zy', bang = true })
     local selection = vim.fn.getreg("z")
+    return selection
+end
+
+local function runTestForSelection()
+    local selection = get_visual_selection()
     vim.cmd("TermExec cmd='djt " .. selection .. "'")
 end
 
 -- Django mapping to run tests easily in visual mode
-vim.keymap.set("v", "<cr><cr>", get_visual_selection)
+vim.keymap.set("v", "<cr><cr>", runTestForSelection)
