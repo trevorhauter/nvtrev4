605e568462408ca5b5aa4104f3816b868078efef
Trevor <trevor@utilityapi.com>
2026-08-05 15:06:13 -0400

    Rebind arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -2,6 +2,6 @@ vim.pack.add({ "https://github.com/otavioschwanck/arrow.nvim" })
 
 require("arrow").setup({
     show_icons = true,
-    leader_key = "<leader>s", -- Recommended to be a single key
+    leader_key = ";", -- Recommended to be a single key
     buffer_leader_key = "m", -- Per Buffer Mappings
 })
